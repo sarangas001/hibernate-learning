4298905b497362e573eda136803e7d3aa0d59c36
saranaga <sarangasama@gmail.com>
2026-08-08 23:16:43 +0530

    Learn: Level 1 Cache in Hibernate

--- HibernateSelective/src/main/java/com/telusko/app/GetRecordApp.java
+++ HibernateSelective/src/main/java/com/telusko/app/GetRecordApp.java
@@ -10,20 +10,30 @@ public class GetRecordApp {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
         Session session = null;
+        Session session2 = null;
 
         try {
             session = sessionFactory.openSession();
-//            Student student = session.get(Student.class, 2);
+            session2 = sessionFactory.openSession();
+            Student student = session.get(Student.class, 1);
+            System.out.println(student);
+            Student student2 = session2.get(Student.class, 1);
+            System.out.println(student2);
+
+            Student student3 = session.get(Student.class, 1);
+            System.out.println(student3);
+
+
 //            Student student = session.load(Student.class, 1);
-            Student student = session.getReference(Student.class, 44);
-            if(student!=null) {
-                System.out.println("ID is : " + student.getSid());
-                System.in.read();
-                System.out.println("Name is : " + student.getsName());
-                System.out.println("City is : " + student.getScity());
-            }else {
-                System.out.println("There is no data/record with id");
-            }
+//            Student student = session.getReference(Student.class, 44);
+//            if(student!=null) {
+//                System.out.println("ID is : " + student.getSid());
+//                System.in.read();
+//                System.out.println("Name is : " + student.getsName());
+//                System.out.println("City is : " + student.getScity());
+//            }else {
+//                System.out.println("There is no data/record with id");
+//            }
 
         }catch (HibernateException e) {
             e.printStackTrace();
@@ -32,6 +42,7 @@ public class GetRecordApp {
         }finally {
             session.close();
             sessionFactory.close();
+            session2.close();
         }
     }
 }
