29145dc10947f4b7280fe7f3c5f14aa6bf59ccf2
saranaga <sarangasama@gmail.com>
2026-08-09 13:39:44 +0530

    Learn: Level 2 Cache in Hibernate (EhCache)

---
+++ HibernateSelective/src/main/java/com/telusko/app/HibernateLevel2Cache.java
@@ -0,0 +1,38 @@
+package com.telusko.app;
+
+import com.telusko.model.Student;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateLevel2Cache {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
+        Session session = null;
+        Session session2 = null;
+
+        try {
+            session = sessionFactory.openSession();
+            session2 = sessionFactory.openSession();
+            Student student = session.get(Student.class, 1);
+            System.out.println(student);
+            Student student2 = session2.get(Student.class, 1);
+            System.out.println(student2);
+
+            Student student3 = session.get(Student.class, 1);
+            System.out.println(student3);
+
+
+        }catch (HibernateException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            session.close();
+            sessionFactory.close();
+            session2.close();
+        }
+    }
+}
--- HibernateSelective/src/main/java/com/telusko/model/Student.java
+++ HibernateSelective/src/main/java/com/telusko/model/Student.java
@@ -1,12 +1,13 @@
 package com.telusko.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Table(name="StudentTable")
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Student
 {
     @Id
