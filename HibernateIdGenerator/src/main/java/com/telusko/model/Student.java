c4aa8695f158d63786cf930bcf91cd4986806d00
saranaga <sarangasama@gmail.com>
2026-08-14 19:32:55 +0530

    Learn: Exploring @GeneratedValue and @SequenceGenerator

---
+++ HibernateIdGenerator/src/main/java/com/telusko/model/Student.java
@@ -0,0 +1,58 @@
+package com.telusko.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="StudentTable")
+public class Student
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "my_seq", sequenceName = "My_OwnSequence", initialValue = 100, allocationSize = 1)
+    private Integer sid;
+
+    @Column(name = "SNAME")
+    private String sName;
+
+    @Column(name = "SCITY")
+    private String scity;
+
+    public Student() {
+        System.out.println("Zero Param Constructor for Hibernate");
+    }
+
+
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getScity() {
+        return scity;
+    }
+
+    public void setScity(String scity) {
+        this.scity = scity;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sid=" + sid +
+                ", sName='" + sName + '\'' +
+                ", scity='" + scity + '\'' +
+                '}';
+    }
+}
